df3695e9cc0b3953575708265bd2faeaec82e85f
liubenwei00 <439474763@qq.com>
2019-11-02 09:30:02 +0800

    Initial commit

---
+++ src/main/java/com/liu/imoocconcurrency/ImoocConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.liu.imoocconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocConcurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liu/imoocconcurrency/ImoocConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.imoocconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
